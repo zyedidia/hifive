2571cbe384c1d0b83a42684de5d97287e58ae0f2
Zachary Yedidia <zyedidia@gmail.com>
2022-04-06 00:06:18 -0700

    Improve interrupt jit compiler

--- programs/intcompiler/intcompiler.c
+++ programs/intcompiler/intcompiler.c
@@ -1,6 +1,7 @@
 #include "riscv.h"
 #include "libfive/uart.h"
 #include "libfive/timer.h"
+#include <string.h>
 
 int count;
 
@@ -14,14 +15,10 @@ void int7() { count += 7; }
 
 typedef void (*intfn_t)();
 
-void hello() {
-    printf("Hello world\n");
-}
-
 #define BENCH(call) \
 { int og_tx = read_ticks(); \
     for (int _i = 0; _i < 10000; _i++) {call;} \
-  printf("tx: %d\n", read_ticks() - og_tx); }
+  printf("ticks: %d\n", read_ticks() - og_tx); }
 
 void slow(intfn_t* fns, int n) {
     for (int i = 0; i < n; i++) {
@@ -36,9 +33,6 @@ void fast(intfn_t* fns, int nfn) {
     code[n++] = riscv_addi(REG_SP, REG_SP, -16);
     code[n++] = riscv_sw(REG_RA, REG_SP, 12);
 
-    /* n += riscv_li(REG_T0, (uint32_t) hello, &code[n]); */
-    /* code[n++] = riscv_jalr(REG_RA, REG_T0, 0); */
-
     for (int i = 0; i < nfn; i++) {
         n += riscv_li(REG_T0, (uint32_t) fns[i], &code[n]);
         code[n++] = riscv_jalr(REG_RA, REG_T0, 0);
@@ -48,20 +42,35 @@ void fast(intfn_t* fns, int nfn) {
     code[n++] = riscv_addi(REG_SP, REG_SP, 16);
     code[n++] = riscv_ret();
 
-    printf("%lx\n", code[0]);
-    printf("%lx\n", code[1]);
-    printf("%lx\n", code[2]);
-    printf("%lx\n", code[3]);
-    printf("%lx\n", code[4]);
-    printf("%lx\n", code[5]);
-    printf("%lx\n", code[6]);
-    printf("%lx\n", code[7]);
-
     for (int i = 0; i < 32; i++) {
         asm volatile ("nop");
     }
 
     void (*fn)(void) = (typeof(fn)) code;
+    BENCH(fn());
+}
+
+void faster(intfn_t* fns, int nfn) {
+    uint32_t fns_copy[nfn * 8];
+
+    for (int i = 0; i < nfn; i++) {
+        memcpy(&fns_copy[i * 8], fns[i], sizeof(uint32_t) * 8);
+    }
+
+    for (int i = 0; i < nfn-1; i++) {
+        uint32_t* code = (uint32_t*) &fns_copy[i * 8];
+        while (*code != riscv_ret()) {
+            code++;
+        }
+        uint32_t insn = riscv_jal(REG_ZERO, (uint32_t) &fns_copy[(i+1) * 8] - (uint32_t) code);
+        *code = insn;
+    }
+
+    for (int i = 0; i < 32; i++) {
+        asm volatile ("nop");
+    }
+
+    void (*fn)(void) = (typeof(fn)) fns_copy;
     BENCH(fn());
 }
 
@@ -76,8 +85,18 @@ int main() {
         int7,
     };
 
-    /* BENCH(slow(fns, 7)); */
+    printf("--- slow ---\n");
+    BENCH(slow(fns, 7));
+    printf("count: %d\n", count);
+
+    printf("--- jit ---\n");
+    count = 0;
     fast(fns, 7);
+    printf("count: %d\n", count);
+
+    printf("--- jump threading ---\n");
+    count = 0;
+    faster(fns, 7);
     printf("count: %d\n", count);
 
     return 0;
